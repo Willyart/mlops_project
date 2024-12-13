a2a752afa08dd006dfcd5aec8316d55cd39982d9
vace23 <valentin.acevedo@yahoo.fr>
2024-12-13 14:40:31 +0100

    Ajout bucket

---
+++ set_env_vars.sh
@@ -0,0 +1,4 @@
+export GCP_PROJECT_ID=mlops-project-a-b-d-g-m
+export GCP_BILLING_ACCOUNT_ID=019C56-FD3926-D305E6
+export GCP_BUCKET_NAME=bucket-mlops-project-a-b-d-g-m
+export GCP_BUCKET_LOCATION=europe-west6
